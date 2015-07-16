f77ab14f6e517db9e9df5daf7f1eb02208fab084
jeffrey-cochran <kvasir.consulting@gmail.com>
2015-07-16 14:08:27 -0400

    Added simple numpy code converting a list to an ndarrray.

--- src/LostKnowledge.py
+++ src/LostKnowledge.py
@@ -3,3 +3,17 @@ Created on Jul 16, 2015
 
 @author: aeames
 '''
+#
+#
+#
+from numpy import asarray
+#
+#
+#
+a   = asarray([[1, 2, 3],
+               [4, 5, 6],
+               [7, 8, 9]])
+#
+#
+#
+print type(a)
