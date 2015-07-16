0e1515ea00a752057d57722d990c1b2c7d80210c
jeffrey-cochran <kvasir.consulting@gmail.com>
2015-07-16 13:47:27 -0400

    Added dummy functions for householder reflections and QR factorization

--- src/Test.py
+++ src/Test.py
@@ -3,3 +3,44 @@ Created on Jul 16, 2015
 
 @author: aeames
 '''
+#
+#
+#
+def H(x,u):
+    return HouseholderReflection(x, u)
+#
+#
+#
+def QR(X):
+    return QRFactorization(X)
+#
+#
+#
+def HouseholderReflection(x, u):
+    '''
+    Inputs:
+        - x, the vector being reflected about a hyperplane
+        - u, a unit vector normal to the hyperplane
+    Outputs:
+        - Reflected_x,    x after reflection about the hyperplane
+    '''
+    #
+    Reflected_x = None
+    #
+    return Reflected_x
+#
+#
+#
+def QRFactorization(X):
+    '''
+    Inputs:
+        - X, an (MxN) rectangular matrix
+    Outputs:
+        - Q, an (NxN) orthogonal matrix
+        - R, an (MxN) upper triangular matrix such that X = QR
+    '''
+    #
+    Q       = None
+    R       = None
+    #
+    return (Q, R)
